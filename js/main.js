28de531325d7335651aa41e6d20dfd915892f443
ALPINISTLAR <niveam424@gmail.com>
2023-05-25 20:11:51 +0200

    add site-header and projects-list

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const elDarkModeToggler = document.querySelector('.dark-mode-toggler');
+
+elDarkModeToggler.addEventListener('click', function () {
+document.body.classList.toggle('dark-mode');
+});
