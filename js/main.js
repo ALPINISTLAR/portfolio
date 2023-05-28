281ca7d198b3c09e041df25267b1a46568c8019e
ALPINISTLAR <niveam424@gmail.com>
2023-05-28 10:22:53 +0200

    add three projects

--- js/main.js
+++ js/main.js
@@ -3,3 +3,12 @@ const elDarkModeToggler = document.querySelector('.dark-mode-toggler');
 elDarkModeToggler.addEventListener('click', function () {
 document.body.classList.toggle('dark-mode');
 });
+
+const elsProjectsListItem = document.querySelectorAll('.projects-list__item');
+const elProjectsButton = document.querySelector('.js-projects-button');
+
+elProjectsButton.addEventListener('click', function () {
+elsProjectsListItem.forEach(function (elProjectListItem) {
+elProjectListItem.classList.remove('projects-item--none')
+})
+});
